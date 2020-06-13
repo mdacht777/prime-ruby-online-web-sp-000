198ac8c3fe42b52df390655b04b4c0b32a4efc1d
Martin Achtermann <mdacht777@gmail.com>
2020-06-13 14:14:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(num)
-   puts num
+   if num>0
    Math.sqrt(num).floor.downto(2).each {|i| return false if (num % i == 0 || num<1)}
    true
+ else
+   false
+ end
 end
